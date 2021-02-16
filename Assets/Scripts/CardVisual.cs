83040da611b1a8fa2687d02357a6e6b92b1db7ad
Andres <andrespablo994@gmail.com>
2021-02-16 11:50:09 -0300

    Card Visual Components started
    Variables only

--- Assets/Scripts/CardVisual.cs
+++ Assets/Scripts/CardVisual.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardVisual : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CardLogic logic;
+    [SerializeField]Image glow;
+    [SerializeField]Image art;
+    [SerializeField]Image rune;
+    [Space]
+    [SerializeField]Text level;
+    [SerializeField]Text health;
+    [SerializeField]Text attack;
+
+
     void Start()
     {
-        
+        logic = GetComponent<CardLogic>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
